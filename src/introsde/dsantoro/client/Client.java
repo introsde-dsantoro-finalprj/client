ab2ff94ba020c35cf4d898ea64a97eb5023be1ac
Daniele Santoro <psyacca@gmail.com>
2016-02-07 12:35:16 +0100

    Add insert person

--- src/introsde/dsantoro/client/Client.java
+++ src/introsde/dsantoro/client/Client.java
@@ -1,6 +1,8 @@
 package introsde.dsantoro.client;
 
 import java.io.IOException;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -10,31 +12,35 @@ import introsde.dsantoro.pcws.Pcws;
 public class Client {
 
 	Pcws pcws;
-	Person person= null;
+	Person person = null;
+	Scanner scanner;
 
-	private static final String RET = "\n";
-	private static final String TAB = "\t";
-	private static final String DASHBOARDMENU_MSG = "--> Please tell me if you want to" + RET
-			+ TAB + "[I]nsert a Person" + RET
-			+ TAB + "[W]ork on Person in the session" + RET
-			+ TAB + "[A]dmin menu" + RET
-			+ TAB + "[Q]uit" + RET;
+	private final String RET = "\n";
+	private final String TAB = "\t";
+
 	private static final String QUIT_MSG = "Quitting. Remember to eat healthy !!!";
+	
+	
 	public Client(Pcws pcws) {
 		this.pcws = pcws;
+		scanner = new Scanner(System.in);
 		dashboardMenu();
 	}
 
 
 
+	private String getPersonId() {
+		return (person == null) ? "NOT YET SET" : person.getId().toString();  
+	}
+
 	private void dashboardMenu() {
 		int ch;
 		boolean quit = false;
 		
-		System.out.println(DASHBOARDMENU_MSG);
+		dashboardMenuMsg();
 		
 		try {
-			while ( ((ch = System.in.read()) != -1) && (quit == false) ) {                
+			while ( ((ch = System.in.read()) != -1) && (quit == false) ) {
 				if (ch != '\n' && ch != '\r') {                    
 					switch((char)ch){
 					case 'i':
@@ -56,9 +62,10 @@ public class Client {
 					case 'Q':
 						System.out.println(QUIT_MSG);
 						quit = true;
+						scanner.close();
 						break;                    
 					}
-					if (!quit) System.out.println(DASHBOARDMENU_MSG);
+					if (!quit) dashboardMenuMsg();
 				}
 
 			}
@@ -66,6 +73,18 @@ public class Client {
 			Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
 		}
 
+	}
+
+
+
+	private void dashboardMenuMsg() {
+		System.out.println(
+				"--> Please tell me if you want to" + RET
+				+ TAB + "[I]nsert a Person" + RET
+				+ TAB + "[W]ork on Person in the session ("+ getPersonId() + ")" + RET
+				+ TAB + "[A]dmin menu" + RET
+				+ TAB + "[Q]uit" + RET
+				);
 	}
 
 
@@ -84,8 +103,30 @@ public class Client {
 
 
 
-	private void insertPerson() {
-		// TODO Auto-generated method stub
-
+	private void insertPerson() {		
+		try {
+			Person p = new Person();
+			System.out.println("--> Please inser Person details:");
+			System.out.println(TAB + "Firstname: ");		
+			p.setFirstname(scanner.next());
+			System.out.println(TAB + "Lastname: ");
+			p.setLastname(scanner.next());
+			//System.out.println(TAB + "Birthday: ");
+			//p.setBirthday(scanner.next());
+			System.out.println(TAB + "email: ");
+			p.setEmail(scanner.next());
+			System.out.println(TAB + "weight: ");
+			p.setWeight(scanner.next());
+			System.out.println(TAB + "height: ");
+			p.setHeight(scanner.next());
+			System.out.println(TAB + "Calories burned per day: ");
+			p.setDaycalories(scanner.nextInt());		
+			person = pcws.createPerson(p);		
+			System.out.println("--> Person created.");	
+		} catch (InputMismatchException e) {
+			System.err.println("--> Person not created. Check last inserted value.");
+		}
+		
+		
 	}
 }
